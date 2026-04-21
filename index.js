d6e8cbee06a1d869696bd1d85f40eea6a1798779
AlexanderGSC <alexander.gsc@gmail.com>
2026-04-21 16:35:35 +0200

    testing CI/CD pipeline. Adding text

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
-  const html = `
-    <h1>Hello World!</h1>
-    <h2>Commits Log</h2>
-    <p>[1]: Added a <b>CI/CD</b> pipeline:<br />
-    Now every change is actually committed to DockerHub and locally managed by Watchtower</p>
-    <p>[2]: Fixed small bugs<p>
-  `;
-  res.send(html);
+    const html = `
+        <h1>Hello World!</h1>
+        <h2>Commits Log</h2>
+        <p>[1]: Added a <b>CI/CD</b> pipeline:<br />
+        Now every change is actually committed to DockerHub and locally managed by Watchtower.</p>
+        <p>[2]: Fixed small bugs.</p>
+    `;
+    res.send(html);
 });
 
-const PORT = 8080
+const PORT = 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
