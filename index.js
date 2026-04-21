f0a051498663394f049daf1c750084786dbe3332
AlexanderGSC <alexander.gsc@gmail.com>
2026-04-21 16:13:37 +0200

    testing CI/CD pipeline

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World! (version 2)</h1>')
+  res.send('<h1>Hello World!</h1>')
+  res.send('<h2>Commits Log</h2>')
+  res.send('<p>[1]: Added a <b>CI/CD</b> pipeline:<br />Now every chage is actually commited to DockerHub and locally managed by Watchtower</p>')
 })
 
 const PORT = 8080
